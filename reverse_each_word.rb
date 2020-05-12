e3d18147e8eb272c5f0083875f8d5a99dfac0812
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-12 20:10:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,25 @@
 require "pry"
 
+def rev(wd)
+  arr = wd.split("") 
+  newa = []
+  arr.each_with_index do |letter, ind| 
+    newind = arr.length -1 - ind 
+    newa[newind] = letter 
+  end 
+  return newa 
+end 
+
+
 def reverse_each_word(sen) 
   arr = sen.split(" ")
   arr.map do |word| 
-    word.reverse 
+    rev(word) 
   end 
   k = arr.join(" ")
   puts k 
 end 
 
+rev("abc")
 reverse_each_word("Hello yo yo!") 
 
