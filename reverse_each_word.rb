c11ef363ff02ea04f7b180c0af6e919b5e7ff30b
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-12 20:06:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,11 @@
 require "pry"
 
 def reverse_each_word(sen) 
-
+  arr = sen.split(" ")
+  arr.collect do |word| 
+    word.reverse 
+  end 
+  arr.join("")
 end 
 
 reverse_each_word("Hello yo yo!") 
